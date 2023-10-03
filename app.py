3712407ade908b6b21b8b7a003f6267d973dd16c
pythonvista <emeterv18@gmail.com>
2023-10-03 04:55:01 +0100

    ffdf

--- app.py
+++ app.py
@@ -43,53 +43,53 @@ async def predict_malware(file: UploadFile):
 
     # print(classifier.predict([[variance,skewness,curtosis,entropy]]))
     new_log_data = pd.read_csv(file.filename)
-    cols="""duration,
-        protocol_type,
-        service,
-        flag,
-        src_bytes,
-        dst_bytes,
-        land,
-        wrong_fragment,
-        urgent,
-        hot,
-        num_failed_logins,
-        logged_in,
-        num_compromised,
-        root_shell,
-        su_attempted,
-        num_root,
-        num_file_creations,
-        num_shells,
-        num_access_files,
-        num_outbound_cmds,
-        is_host_login,
-        is_guest_login,
-        count,
-        srv_count,
-        serror_rate,
-        srv_serror_rate,
-        rerror_rate,
-        srv_rerror_rate,
-        same_srv_rate,
-        diff_srv_rate,
-        srv_diff_host_rate,
-        dst_host_count,
-        dst_host_srv_count,
-        dst_host_same_srv_rate,
-        dst_host_diff_srv_rate,
-        dst_host_same_src_port_rate,
-        dst_host_srv_diff_host_rate,
-        dst_host_serror_rate,
-        dst_host_srv_serror_rate,
-        dst_host_rerror_rate,
-        dst_host_srv_rerror_rate"""
-    columns=[]
-    for c in cols.split(','):
-        if(c.strip()):
-            columns.append(c.strip())
+    # cols="""duration,
+    #     protocol_type,
+    #     service,
+    #     flag,
+    #     src_bytes,
+    #     dst_bytes,
+    #     land,
+    #     wrong_fragment,
+    #     urgent,
+    #     hot,
+    #     num_failed_logins,
+    #     logged_in,
+    #     num_compromised,
+    #     root_shell,
+    #     su_attempted,
+    #     num_root,
+    #     num_file_creations,
+    #     num_shells,
+    #     num_access_files,
+    #     num_outbound_cmds,
+    #     is_host_login,
+    #     is_guest_login,
+    #     count,
+    #     srv_count,
+    #     serror_rate,
+    #     srv_serror_rate,
+    #     rerror_rate,
+    #     srv_rerror_rate,
+    #     same_srv_rate,
+    #     diff_srv_rate,
+    #     srv_diff_host_rate,
+    #     dst_host_count,
+    #     dst_host_srv_count,
+    #     dst_host_same_srv_rate,
+    #     dst_host_diff_srv_rate,
+    #     dst_host_same_src_port_rate,
+    #     dst_host_srv_diff_host_rate,
+    #     dst_host_serror_rate,
+    #     dst_host_srv_serror_rate,
+    #     dst_host_rerror_rate,
+    #     dst_host_srv_rerror_rate"""
+    # columns=[]
+    # for c in cols.split(','):
+    #     if(c.strip()):
+    #         columns.append(c.strip())
 
-    columns.append('target')
+    # columns.append('target')
     attacks_types = {
     'normal': 'normal',
     'back': 'dos',
@@ -115,11 +115,11 @@ async def predict_malware(file: UploadFile):
     'warezclient': 'r2l',
     'warezmaster': 'r2l',
     }
-    df = pd.read_csv(new_log_data,names=columns)
-    df['Attack Type'] = df.target.apply(lambda r:attacks_types[r[:-1]])
-    df = df.drop(['target'], axis=1)
+    # df = pd.read_csv(new_log_data,names=columns)
+    # df['Attack Type'] = df.target.apply(lambda r:attacks_types[r[:-1]])
+    # df = df.drop(['target'], axis=1)
     return {
-        'prediction': df
+        'prediction': new_log_data
     }
 
 
